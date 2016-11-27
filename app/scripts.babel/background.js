3799a36b496290ba521613ea85ae43f20b84fa82
guozhen <gravitywp@gmail.com>
2016-11-27 18:09:59 +0800

    captrue subtitle url && parse xmltext

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -8,4 +8,12 @@ chrome.tabs.onUpdated.addListener(tabId => {
   chrome.pageAction.show(tabId);
 });
 
+chrome.webRequest.onCompleted.addListener((details) => {
+  if( (details.url.indexOf("timedtext") !== -1) && (details.url.indexOf("from_extension") === -1) ) {
+    chrome.tabs.sendMessage(details.tabId, {textUrl: details.url}, response => {
+      console.log(response);
+    });
+  }
+}, {urls: ["*://www.youtube.com/*"]});
+
 console.log('\'Allo \'Allo! Event Page for Page Action');
--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -1,3 +1,14 @@
 'use strict';
 
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  console.log(request);
+  fetch(request.textUrl + "&from_extension=true").then(response => response.text()).then(text => {
+    const parser = new DOMParser();
+    const xmlDoc = parser.parseFromString(text, "text/xml");
+    return xmlDoc;
+  }).then(xmlDoc => console.dir(xmlDoc));
+  sendResponse({status: "ok"});
+});
+
+
 console.log('\'Allo \'Allo! Content script');
